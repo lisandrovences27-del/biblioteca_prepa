29f722f99093c4a7237166fa858c7954c82476ab
yuritzi valadez <valadezaguilaryuritzitrones@gmail.com>
2026-05-26 10:26:39 -0600

    Agregue el login

---
+++ frontend/src/vistas/DashboardAdmin1.jsx
@@ -0,0 +1,141 @@
+import "../App.css";
+
+function DashboardAdmin1() {
+
+  return (
+
+    <div className="dashboard">
+
+      {/* ===== SIDEBAR ===== */}
+      <aside className="sidebar">
+
+        {/* Logo */}
+        <div>
+
+          <h2 className="logo-dashboard">
+            PrestaCetis
+          </h2>
+
+          <p className="logo-subtitle">
+            Sistema de préstamos
+          </p>
+
+        </div>
+
+
+
+        {/* Menú */}
+        <ul className="menu">
+
+          <li className="active-menu">
+            🏠 Dashboard
+          </li>
+
+          <li>
+            📚 Libros
+          </li>
+
+          <li>
+            📦 Préstamos
+          </li>
+
+          <li>
+            👨‍🎓 Alumnos
+          </li>
+
+          <li>
+            📊 Reportes
+          </li>
+
+          <li>
+            ⚙️ Configuración
+          </li>
+
+        </ul>
+
+
+
+        {/* Usuario abajo */}
+        <div className="user-box">
+
+          <div className="user-circle">
+            A
+          </div>
+
+          <div>
+
+            <h4>
+              Administrador
+            </h4>
+
+            <p>
+              admin@cetis.edu.mx
+            </p>
+
+          </div>
+
+        </div>
+
+      </aside>
+
+
+
+     {/* ===== CONTENIDO ===== */}
+<main className="main-content">
+
+  {/* ===== TOPBAR ===== */}
+  <div className="topbar">
+
+    {/* Parte izquierda */}
+    <div>
+
+      <h1>
+        Bienvenido, Administrador!!
+      </h1>
+
+      <p>
+        Gestiona préstamos, libros y alumnos.
+      </p>
+
+    </div>
+
+
+
+    {/* Parte derecha */}
+    <div className="topbar-right">
+
+      {/* Buscador */}
+      <input
+        type="text"
+        placeholder="Buscar..."
+        className="search-input"
+      />
+
+
+
+      {/* Notificación */}
+      <div className="icon-box">
+        🔔
+      </div>
+
+
+
+      {/* Usuario */}
+      <div className="profile-box">
+
+        <div className="profile-circle">
+          A
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</main>
+</div>
+);
+}
+
+export default DashboardAdmin1;
