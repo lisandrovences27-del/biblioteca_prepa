f4e4c2a69c29629bb442165a519a033e2b7fff8e
Lisandro Vences <lisandrovences27@gmail.com>
2026-08-18 09:36:39 -0600

    arranque sel servidor listo

---
+++ arrancar_servidor.bat
@@ -0,0 +1,6 @@
+@echo off
+title Servidor Biblioteca Cetis 120
+echo Iniciando el servidor de la biblioteca...
+echo Por favor, NO cierres esta ventana. Si la cierras, el sistema se apagara.
+cd /d "C:\xampp\htdocs\biblioteca_prepa"
+npm run dev
